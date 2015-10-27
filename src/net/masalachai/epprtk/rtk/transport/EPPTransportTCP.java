08a4460f680861ef65017a4b46513bbd7b73b27c
masalachai <code@masalachai.net>
2015-10-27 19:45:51 +0800

    added licence header to EPPTransportTCP

--- src/net/masalachai/epprtk/rtk/transport/EPPTransportTCP.java
+++ src/net/masalachai/epprtk/rtk/transport/EPPTransportTCP.java
@@ -1,8 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+**
+** EPP RTK Java
+** Copyright (C) 2001-2002, Tucows, Inc.
+** Copyright (C) 2003, Liberty RMS
+**
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this library; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**
+*/
+
 package net.masalachai.epprtk.rtk.transport;
 
 import static com.tucows.oxrs.epprtk.rtk.RTKBase.DEBUG_LEVEL_THREE;
